8543454bba0b33eca6172d5aae1821b109634e44
Vincent Noël <vincent.noel@curie.fr>
2025-04-23 15:52:41 -0300

    Fixing Clobber issues on windows: moving problematic files to subdirectory

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -19,3 +19,11 @@ cmake --build build --target install
 del /q %PREFIX%\Library\share\cmake\Modules\FindLIBXML.cmake
 del /q %PREFIX%\Library\share\cmake\Modules\FindBZ2.cmake
 del /q %PREFIX%\Library\share\cmake\Modules\FindZLIB.cmake
+
+mkdir %PREFIX%\Library\share\libsbml\
+move /y %PREFIX%\Library\COPYING.txt %PREFIX%\Library\share\libsbml\COPYING.txt
+move /y %PREFIX%\Library\FUNDING.txt %PREFIX%\Library\share\libsbml\FUNDING.txt
+move /y %PREFIX%\Library\LICENSE.txt %PREFIX%\Library\share\libsbml\LICENSE.txt
+move /y %PREFIX%\Library\NEWS.txt %PREFIX%\Library\share\libsbml\NEWS.txt
+move /y %PREFIX%\Library\README.md %PREFIX%\Library\share\libsbml\README.md
+move /y %PREFIX%\Library\VERSION.txt %PREFIX%\Library\share\libsbml\VERSION.txt
