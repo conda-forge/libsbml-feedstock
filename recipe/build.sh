e24a1527995ac16eda1b67c67f832abd31444d95
Vincent Noël <vincent.noel@curie.fr>
2025-11-17 14:50:57 -0300

    Trying to fix lib install dir

--- recipe/build.sh
+++ recipe/build.sh
@@ -15,6 +15,7 @@ elif [[ ${target_platform} == linux-*64 ]]; then
 fi
 
 cmake ${CMAKE_ARGS} \
+      -DCMAKE_INSTALL_LIBDIR=${PREFIX}/lib \
       -DCMAKE_BUILD_TYPE=Release \
       -DBUILD_SHARED_LIBS=ON \
       -DCMAKE_CXX_STANDARD_LIBRARIES=-lxml2 \
